5807eb351e4b685d99ec26d3f2c66a9618bbe89a
Volodymyr <uuforreasonsunknownuu@gmail.com>
2024-12-19 19:25:08 +0200

    solution

--- car/serializers.py
+++ car/serializers.py
@@ -1,5 +1,48 @@
 from rest_framework import serializers
 
+from car.models import Car
+
 
 class CarSerializer(serializers.Serializer):
-    pass
+    id = serializers.IntegerField(read_only=True)
+    manufacturer = serializers.CharField(required=True, max_length=64)
+    model = serializers.CharField(required=True, max_length=64)
+    horse_powers = serializers.IntegerField()
+    is_broken = serializers.BooleanField(default=False)
+    problem_description = serializers.CharField(
+        required=False,
+        allow_blank=True,
+        allow_null=True
+    )
+
+    def validate_horse_powers(self, value):
+        if value < 1 or value > 1914:
+            raise serializers.ValidationError("incorrect horse power value")
+        return value
+
+    def create(self, validated_data):
+        return Car.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.manufacturer = validated_data.get(
+            "manufacturer",
+            instance.manufacturer
+        )
+        instance.model = validated_data.get(
+            "model",
+            instance.model
+        )
+        instance.horse_powers = validated_data.get(
+            "horse_powers",
+            instance.horse_powers
+        )
+        instance.is_broken = validated_data.get(
+            "is_broken",
+            instance.is_broken
+        )
+        instance.problem_description = validated_data.get(
+            "problem_description",
+            instance.problem_description
+        )
+        instance.save()
+        return instance
--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+import json
 from car.models import Car
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serializer = CarSerializer(car)
+    return json.dumps(
+        serializer.data,
+        separators=(",", ":")
+    ).encode("utf-8")
 
 
-def deserialize_car_object(json: bytes) -> Car:
-    pass
+def deserialize_car_object(json_bytes: bytes) -> Car:
+    json_str = json_bytes.decode("utf-8")
+    car_data = json.loads(json_str)
+    serializer = CarSerializer(data=car_data)
+    serializer.is_valid(raise_exception=True)
+    return serializer.save()
